ea5af53f87f35cdfb611f17b5a8311bbf5d237b0
Sunnatilla-Jumadullayev <jumaduullayevsunnat@gmail.com>
2024-11-08 07:17:27 +0500

    qwery

--- src/component/Router.jsx
+++ src/component/Router.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
-import About from "../pages/about/about";
+import About from "../pages/about/About.jsx";
 import Contact from "../pages/contact/Contact";
 import NotFount from "../pages/notFount/NotFount";
 import Layout from "../layout/Layout";
