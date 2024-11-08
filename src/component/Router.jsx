d638d25da29f168324499c057bcd7d0b6af5e650
Sunnatilla-Jumadullayev <jumaduullayevsunnat@gmail.com>
2024-11-08 07:27:44 +0500

    error

--- src/component/Router.jsx
+++ src/component/Router.jsx
@@ -3,12 +3,11 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
 import About from "../pages/about/About.jsx";
 import Contact from "../pages/contact/Contact";
-import NotFount from "../pages/notFount/NotFount";
+import NotFount from "../pages/notfount/Notfount";
 import Layout from "../layout/Layout";
 import Delivery from "../pages/delivery/Delivery";
 import Return from "../pages/return/Return";
 import Guarantees from "../pages/guarantees/Guarantees";
-import NotFount from "./notFount/NotFount.jsx";
 
 const Router = () => {
   return (
